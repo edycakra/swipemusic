ce4a8b6c10ac19729a522abf538b5f1d1381d59a
edycakra <edycakrasinaga@gmail.com>
2020-07-17 18:51:16 +0700

    bgcolor change

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -14,10 +14,12 @@ export default function Home() {
   //mutable state
   const [state, setState] = useState({
     cardsPan: new Animated.ValueXY(),
+    cardsStackedAnim: new Animated.Value(0),
+    currentIndex: 0,
   });
 
   //declaring available states
-  const { cardsPan } = state;
+  const { cardsPan, cardsStackedAnim, currentIndex } = state;
 
   //function to handle pan respond
   const cardsPanResponder = PanResponder.create({
@@ -29,7 +31,25 @@ export default function Home() {
       cardsPan.setValue({ x: gestureState.dx, y: cardsPan.y });
     },
     onPanResponderTerminationRequest: () => false,
-    onPanResponderRelease: (event, gestureState) => {},
+    onPanResponderRelease: (event, gestureState) => {
+      //translationX back to 0
+      Animated.timing(cardsPan, {
+        toValue: 0,
+        duration: 300,
+        useNativeDriver: false,
+      }).start();
+
+      //interpolate values
+      Animated.timing(cardsStackedAnim, { toValue: 1, duration: 300 }).start(
+        () => {
+          //reset to 0 when animation ends
+          cardsStackedAnim.setValue(0);
+
+          //increment the card postion after animation is ended
+          setState({ ...state, currentIndex: currentIndex + 1 });
+        }
+      );
+    },
   });
 
   //styling
@@ -39,7 +59,7 @@ export default function Home() {
     position: "absolute",
     zIndex: 1,
     bottom: 120,
-    backgroundColor: COLORS[2],
+    backgroundColor: COLORS[(currentIndex + 2) % 3],
     opacity: 0.3,
     transform: [{ scale: 0.8 }],
   };
@@ -50,7 +70,7 @@ export default function Home() {
     position: "absolute",
     zIndex: 2,
     bottom: 60,
-    backgroundColor: COLORS[1],
+    backgroundColor: COLORS[(currentIndex + 1) % 3],
     opacity: 0.6,
     transform: [{ scale: 0.9 }],
   };
@@ -61,7 +81,7 @@ export default function Home() {
     position: "absolute",
     zIndex: 3,
     bottom: 0,
-    backgroundColor: COLORS[0],
+    backgroundColor: COLORS[currentIndex % 3],
     opacity: 1,
     transform: [{ translateX: cardsPan.x }, { scale: 1.0 }],
   };
